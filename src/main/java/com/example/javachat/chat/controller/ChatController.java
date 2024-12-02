37c41664ca9d070f648c6cf0f937f32108bc05c3
shhwang0930 <shhwang0930@gmail.com>
2024-12-02 17:42:42 +0900

    redis update

--- src/main/java/com/example/javachat/chat/controller/ChatController.java
+++ src/main/java/com/example/javachat/chat/controller/ChatController.java
@@ -1,21 +1,29 @@
 package com.example.javachat.chat.controller;
 
 import com.example.javachat.chat.dto.MessageDTO;
+import com.example.javachat.chat.pubsub.RedisPublisher;
+import com.example.javachat.chat.repository.ChatRoomRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.stereotype.Controller;
 
 @RequiredArgsConstructor
 @Controller
 public class ChatController {
 
-    private final SimpMessageSendingOperations messagingTemplate;
+    private final RedisPublisher redisPublisher;
+    private final ChatRoomRepository chatRoomRepository;
 
+    /**
+     * websocket "/pub/chat/message"로 들어오는 메시징을 처리한다.
+     */
     @MessageMapping("/chat/message")
     public void message(MessageDTO message) {
-        if (MessageDTO.MessageType.ENTER.equals(message.getType()))
+        if (MessageDTO.MessageType.ENTER.equals(message.getType())) {
+            chatRoomRepository.enterChatRoom(message.getRoomId());
             message.setMessage(message.getSender() + "님이 입장하셨습니다.");
-        messagingTemplate.convertAndSend("/sub/chat/room/" + message.getRoomId(), message);
+        }
+        // Websocket에 발행된 메시지를 redis로 발행한다(publish)
+        redisPublisher.publish(chatRoomRepository.getTopic(message.getRoomId()), message);
     }
 }
--- src/main/java/com/example/javachat/chat/dto/RoomDTO.java
+++ src/main/java/com/example/javachat/chat/dto/RoomDTO.java
@@ -3,36 +3,16 @@ package com.example.javachat.chat.dto;
 
 import lombok.Data;
 
+import java.io.Serializable;
 import java.util.UUID;
 
 @Data
-public class RoomDTO {
+public class RoomDTO implements Serializable {
+
+    private static final long serialVersionUID = 6494678977089006639L;
+
     private String roomId;
     private String name;
-    //private Set<WebSocketSession> sessions = new HashSet<>();
-
-    /*@Builder
-    public RoomDTO(String roomId, String name) {
-        this.roomId = roomId;
-        this.name = name;
-    }
-
-    public void handleActions(WebSocketSession session, MessageDTO chatMessage, ChatService chatService) {
-        if (chatMessage.getType().equals(MessageDTO.MessageType.ENTER)) {
-            sessions.add(session);
-            chatMessage.setMessage(chatMessage.getSender() + "님이 입장했습니다.");
-        }
-        if (chatMessage.getType().equals(MessageDTO.MessageType.TALK)) {
-            sessions.add(session);
-            chatMessage.setMessage(chatMessage.getSender() + ": "+chatMessage.getMessage());
-        }
-        sendMessage(chatMessage, chatService);
-    }
-
-    public <T> void sendMessage(T message, ChatService chatService) {
-        sessions.parallelStream().forEach(session -> chatService.sendMessage(session, message));
-    }*/
-
 
     public static RoomDTO create(String name) {
         RoomDTO chatRoom = new RoomDTO();
